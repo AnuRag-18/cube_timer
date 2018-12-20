b309c05f9bf419e27a969527f32c6fc9875d74cf
AnuRag-18 <madhavarapurao720@gmail.com>
2018-12-20 15:51:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/cubetimer',
+// { useNewUrlParser: true },()=>{
+//    console.log("Database connected");
+// });
+// mongoose.Promise = global.Promise;
+
+const timer = require('./api/routes/timer');
+app.use('/timer',timer);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status =404;
+    next(error);
+ });
+ 
+ app.use((error,req,res,next)=>{
+   res.status(error.status || 500);
+   res.json({
+     error:{
+       message:error.message
+     }
+   });
+ });
+ 
+ module.exports = app;
